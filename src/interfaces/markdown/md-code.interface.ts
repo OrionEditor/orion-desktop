e6511c43df0251cbb454ef8198f1c191df50649b
CandyPopsWorld <108881546+CandyPopsWorld@users.noreply.github.com>
2025-05-15 01:16:32 +0300

    Create markdown code parser

---
+++ src/interfaces/markdown/md-code.interface.ts
@@ -0,0 +1,6 @@
+export interface CodeBlock {
+    language: string | null;
+    code: string;
+    startIndex: number;
+    endIndex: number;
+}
---
+++ src/services/Parsers/md-code.parser.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {CodeBlock} from "../../interfaces/markdown/md-code.interface";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MarkdownCodeParserService {
+    extractCodeBlocks(markdown: string): CodeBlock[] {
+        const codeBlocks: CodeBlock[] = [];
+        const codeBlockRegex = /```(\w*)\n([\s\S]*?)\n```/g;
+        let match;
+
+        while ((match = codeBlockRegex.exec(markdown)) !== null) {
+            const language = match[1] || null;
+            const code = match[2];
+            const startIndex = match.index;
+            const endIndex = match.index + match[0].length;
+
+            codeBlocks.push({
+                language,
+                code,
+                startIndex,
+                endIndex
+            });
+        }
+
+        return codeBlocks;
+    }
+}
