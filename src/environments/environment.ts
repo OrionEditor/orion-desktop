f12824a1e1be4a04e6961860ab7eab3e627b8e45
CandyPopsWorld <108881546+CandyPopsWorld@users.noreply.github.com>
2025-06-23 15:35:59 +0300

    New v 0.1.6

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1',
+    apiUrl: 'http://127.0.0.1:8080',
     //webSocketUrl: 'ws://localhost',
     endpoints:{
         auth: {
